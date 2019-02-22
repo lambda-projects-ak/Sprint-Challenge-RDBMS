79fdbdd27755bc21ca43ce99e9e32d45727f0e4b
Kingaa6 <Kingaa6@gmail.com>
2019-02-22 08:32:29 -0800

    Add migrations

--- data/migrations/20190222081734_project.js
+++ data/migrations/20190222081734_project.js
@@ -1,8 +1,16 @@
-
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('project', table => {
+    table.increments();
+    table
+      .string('name', 128)
+      .unique()
+      .notNullable();
+    table.text('description').notNullable();
+    table.boolean('completed').notNullable();
+    tables.timestamps(true, true);
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTableIfExists('project');
 };
--- data/migrations/20190222081851_action.js
+++ data/migrations/20190222081851_action.js
@@ -1,8 +1,21 @@
-
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('action', table => {
+    table.increments();
+    table.text('description').notNullable();
+    table.text('notes');
+    table.boolean('completed').notNullable();
+    table
+      .integer('fk')
+      .unsigned()
+      .notNullable()
+      .references('id')
+      .inTable('project')
+      .onDelete('NO ACTION')
+      .onUpdate('CASCADE');
+    table.timestamps(true, true);
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTableIfExists('action');
 };
