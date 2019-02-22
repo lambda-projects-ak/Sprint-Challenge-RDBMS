a1105562938dad32835c0fe6ed539795a1cb9d72
Kingaa6 <Kingaa6@gmail.com>
2019-02-22 08:10:40 -0800

    Update knex config

--- knexfile.js
+++ knexfile.js
@@ -1,10 +1,16 @@
 // Update with your config settings.
 
 module.exports = {
-
   development: {
     client: 'sqlite3',
+    useNullAsDefault: true,
     connection: {
       filename: './data/dev.sqlite3'
     }
-  }
+  },
+  migrations: {
+    directory: './data/migrations',
+    tableName: 'dbmigrations'
+  },
+  seeds: { directory: './data/seeds' }
+};
